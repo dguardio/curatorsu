bc0a2af4d82abf6b1fc1499817b3ba71cf205eec
Akinwoleola Opeyemi <akinwoleolaopeyemi@gmail.com>
2016-04-02 13:16:48 +0000

    Reconfigure App Navbars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def studyzone
+  	@user = User.course.mods
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :curatorxes
-  devise_for :curators
+  root 'landings#index'
   get 'landings/index'
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :curatorxes
+  devise_for :curators  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".  resources :users, :only => [:show]
   # You can have the root of your site routed with "root"
-  root 'landings#index'
+  
   get 'curators/:id', to: "curators#show", as: 'curator'
   get 'curatorxes/:id', to: "curatorxes#show", as: 'curatorx'
   get 'users/:id', to: "users#show", as: 'user'
