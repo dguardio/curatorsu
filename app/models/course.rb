10e43d91611167c05aa033aa13f724ac9eeda344
Akinwoleola Opeyemi <akinwoleolaopeyemi@gmail.com>
2016-04-08 00:00:37 +0000

    Fixing Active Admin

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,5 @@
 class Course < ActiveRecord::Base
 	has_and_belongs_to_many :users
-	belongs_to :curator
 	has_many :modules, :class_name => "Mod"
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     has_and_belongs_to_many :courses
 
+    enum role: [:user, :curatorx, :curator]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
